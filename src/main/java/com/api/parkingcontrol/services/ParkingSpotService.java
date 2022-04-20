7bbbe7d464483fb0eca1b53009426a80e46e60fa
Paulo Henrique Wilhelms <paulo.wilhelms@hotmail.com>
2022-04-20 13:14:18 -0300

    Atualizado ParkingSpotService

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,4 +1,14 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
 }
