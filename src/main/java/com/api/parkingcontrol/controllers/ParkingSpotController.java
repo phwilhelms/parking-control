ef9a19bd583ee787c43182eed52ef703ba9b614a
Paulo Henrique Wilhelms <paulo.wilhelms@hotmail.com>
2022-04-25 13:06:04 -0300

    Criado ParkingSpotDto

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -1,5 +1,6 @@
 package com.api.parkingcontrol.controllers;
 
+import com.api.parkingcontrol.services.ParkingSpotService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/parking-spot")
 public class ParkingSpotController {
+
+    final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+
 
 }
